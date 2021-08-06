eb8ca033743d8dd89fda68b4b35f98a71eb2d97d
dimiksonkha <sualiman.haque@gmail.com>
2021-08-06 17:42:07 +0600

    activity message updated!

--- src/main/java/com/shifan/dailyactivitymanager/controllers/HomeController.java
+++ src/main/java/com/shifan/dailyactivitymanager/controllers/HomeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  
 @Controller
 public class HomeController {
-    String activityMessage = "Welcome to Daily Activity Manager!"; 
+    String activityMessage = "Welcome to Daily Activity Manager!!!"; 
 
    
     @RequestMapping("/")
