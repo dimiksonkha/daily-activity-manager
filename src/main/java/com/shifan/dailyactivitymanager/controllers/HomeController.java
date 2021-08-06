310c993e8cf20207d9fc1f0f04fe068fc1f0abe3
dimiksonkha <sualiman.haque@gmail.com>
2021-08-06 17:35:43 +0600

    initial commit

--- src/main/java/com/shifan/dailyactivitymanager/controllers/HomeController.java
+++ src/main/java/com/shifan/dailyactivitymanager/controllers/HomeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  
 @Controller
 public class HomeController {
-    String message = "Hello World"; 
+    String message = "Welcome to Daily Activity Manager!"; 
 
    
     @RequestMapping("/")
