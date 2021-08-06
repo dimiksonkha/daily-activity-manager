a518bcf8ef2739a0c37b93ca570d17d8bb5980c2
dimiksonkha <sualiman.haque@gmail.com>
2021-08-06 17:20:16 +0600

    First Local  Commit

---
+++ src/main/java/com/shifan/dailyactivitymanager/controllers/HomeController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shifan.dailyactivitymanager.controllers;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author dimiksonkha
+ */
+ 
+@Controller
+public class HomeController {
+    String message = "Hello World"; 
+
+   
+    @RequestMapping("/")
+    public String showHomePage(Model model) {
+      
+        
+        model.addAttribute("message", message); 
+     
+        return "index";
+               
+	}
+    
+    
+}
